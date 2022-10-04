271bdfc16f707e13ab2793c0513b0fa886ecd0eb
David Podzimek <davidpodzimek1@gmail.com>
2022-10-04 15:56:50 -0500

    ssh test app

---
+++ HaLow.Console.SSH.Test/Program.cs
@@ -0,0 +1,24 @@
+﻿using Renci.SshNet;
+
+string user = "pi";
+string pass = "raspberry";
+string host = "192.168.2.42";
+
+//Set up the SSH connection
+        
+using (var client = new SshClient(host, user, pass))
+{
+    //Start the connection
+    
+    client.Connect();
+    Console.WriteLine(client.IsConnected);
+   /* Console.WriteLine("cd");
+    client.RunCommand("cd /home/pi/nrc_pkg/script/");
+    Console.WriteLine(
+        client.RunCommand("ls").Result);*/
+    var output = client.RunCommand("cd /home/pi/nrc_pkg/script/ && ./start.py 4 3 US 0 && sudo wpa_cli status"); //start.py 4 3 US 0
+    Console.WriteLine(output.Result);
+    Console.WriteLine(output.Error);
+    client.Disconnect();
+    
+}
