c1d0c2c0b7585a13bd800e61dec35623f81c6ccb
David Podzimek <davidpodzimek1@gmail.com>
2022-10-12 15:42:19 -0500

    added Commands

---
+++ HaLow.Raspbian/Commands.cs
@@ -0,0 +1,7 @@
+﻿namespace HaLow.Raspbian;
+
+public static class Commands
+{
+    public const string IFCONFIG_WLAN0 = "sudo /sbin/ifconfig wlan0";
+    public const string MPATH_WLAN0 = "sudo iw dev wlan0 mpath dump";
+}
