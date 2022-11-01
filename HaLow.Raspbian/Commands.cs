b7baaad7639bb514379bf3e224728371a49c14b0
David Podzimek <davidpodzimek1@gmail.com>
2022-11-01 13:51:20 -0500

    added StartComponent.razor, SshLoginComponent.razor

--- HaLow.Raspbian/Commands.cs
+++ HaLow.Raspbian/Commands.cs
@@ -4,4 +4,9 @@ public static class Commands
 {
     public const string IFCONFIG_WLAN0 = "sudo /sbin/ifconfig wlan0";
     public const string MPATH_WLAN0 = "sudo iw dev wlan0 mpath dump";
+    public const string REBOOT = "sudo reboot";
+    public const string WPA_STATUS = "sudo wpa_cli status";
+    
+    public const string NRC_PATH = "cd nrc_pkg/script/";
+    
 }
